6638c0ad6bbccc2cdb0e13e9bb5d897be590808d
xiongmeihui <xiongmeihui@youzan.com>
2019-08-26 09:21:34 +0800

    recast简单使用

--- parser.js
+++ parser.js
@@ -1,5 +1,5 @@
-const { parse, print } = require('recast');
-
+const { parse, print, types, prettyPrint } = require('recast');
+const { variableDeclaration, variableDeclarator, functionExpression } = types.builders;
 const code = [
   "function add(a, b) {",
   "  return a +",
@@ -9,10 +9,21 @@ const code = [
 ].join("\n");
 
 const ast = parse(code);
+const add = ast.program.body[0];
+
+// 函数申明改变量声明
+ast.program.body[0] = variableDeclaration("const", [
+  variableDeclarator(add.id, functionExpression(
+    null, // Anonymize the function expression.
+    add.params,
+    add.body
+  ))
+]);
 
 //console.log(ast);
-console.log(print(ast).code === code);
+//console.log(print(ast).code === code);
 //console.log(ast.program.body[0]);
 // const output = print(ast).code;
+const output = prettyPrint(ast, { tabWidth: 2 }).code
 
-// console.log(output);
+console.log(output);
